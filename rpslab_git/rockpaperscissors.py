823043e127c7a5d563fbf23639336b310b8dc96f
Michael <michaelrharding34@gmail.com>
2025-02-06 09:34:16 -0500

    commit 3: add opponent

--- rpslab_git/rockpaperscissors.py
+++ rpslab_git/rockpaperscissors.py
@@ -1,4 +1,7 @@
 #directory name: rpslab_git
 import random as rand
 
-choice = input("Which do you choose? \n1. rock\n2. paper\n3. scissors")
+choice = input("Which do you choose? \n1. rock\n2. paper\n3. scissors\n")
+
+opponentchoice = rand.choice(["rock", "paper", "scissors"])
+print(opponentchoice)
