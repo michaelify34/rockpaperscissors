9f28132a8f37754d95be1993937672e6a4d2d3b7
Michael <michaelrharding34@gmail.com>
2025-02-06 09:37:44 -0500

    commit 4

--- rpslab_git/rockpaperscissors.py
+++ rpslab_git/rockpaperscissors.py
@@ -5,3 +5,11 @@ choice = input("Which do you choose? \n1. rock\n2. paper\n3. scissors\n")
 
 opponentchoice = rand.choice(["rock", "paper", "scissors"])
 print(opponentchoice)
+
+if choice == "rock":
+    if opponentchoice == "rock":
+        print("Tie")
+    elif opponentchoice == "paper":
+        print("You lose!")
+    elif opponentchoice == "scissors":
+        print("You win!")
